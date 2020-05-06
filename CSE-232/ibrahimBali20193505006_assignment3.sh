9df8ca6e2f5e9fa993b7d28a298ce59ff7e2a178
İbrahim Bali <balii@pm.me>
2020-05-07 01:49:19 +0300

    feat: add compare and sum

--- CSE-232/ibrahimBali20193505006_assignment3.sh
+++ CSE-232/ibrahimBali20193505006_assignment3.sh
@@ -17,11 +17,71 @@ newline_at_eof() {
 }
 
 add_newline_if_missing() {
+
     if newline_at_eof $1; then
         echo
     else
         printf "\n" >>$1
     fi
+}
+
+compare_the_files_and_add_lines() {
+    local filenames=($1 $2)
+
+    for f in ${filenames[@]}; do
+        add_newline_if_missing $f
+    done
+
+    ((number_of_lines_1_has_more_than_2 = $(wc -l <${filenames[0]}) - $(wc -l <${filenames[1]})))
+
+    # echo $number_of_lines_1_has_more_than_2
+
+    local file_that_misses_lines_index=1
+    if [ $number_of_lines_1_has_more_than_2 -lt 0 ]; then
+        ((number_of_lines_1_has_more_than_2 = -$number_of_lines_1_has_more_than_2))
+        file_that_misses_lines_index=0
+    fi
+
+    local file_that_misses_lines=${filenames[${file_that_misses_lines_index}]}
+
+    # TODO write to file use function
+    echo "File $file_that_misses_lines missing $number_of_lines_1_has_more_than_2 entries." >compare.txt
+
+    # (you can write any product name with its price)
+    random_line=$(head -n 1 $file_that_misses_lines)
+
+    until [ $number_of_lines_1_has_more_than_2 -eq 0 ]; do
+        echo $random_line >>$file_that_misses_lines
+        let number_of_lines_1_has_more_than_2-=1
+    done
+
+    return 0
+}
+
+is_sum_odd_or_even() {
+    local sum=0
+
+    local currency
+    while IFS= read -r line; do
+        ((sum += "$(echo $line | sed -E 's/.+\s([0-9]+)\s.+/\1/g')"))
+        currency=${line##* }
+    done <$1
+
+    local market_name="$(echo $1 | sed -E 's/(.*)\.[^.]+/\1/g')"
+
+    local is_even_or_odd
+    if (($sum % 2)); then
+        is_even_or_odd="odd"
+    else
+        is_even_or_odd="even"
+    fi
+
+    # echo $is_even_or_odd
+
+    echo "$market_name market products sum is $sum $currency and it's $is_even_or_odd." >>$2
+    # echo $market_name
+    # echo $sum
+    return 0
 }
 
 main() {
@@ -32,34 +92,13 @@ main() {
         return 2
     fi
 
-    # wc -l <${filenames[0]}
-    # wc -l <${filenames[1]}
+    compare_the_files_and_add_lines ${filenames[0]} ${filenames[1]}
+
+    local odd_even_filename="odd_even.txt"
+    rm $odd_even_filename
 
     for f in ${filenames[@]}; do
-        add_newline_if_missing $f
-    done
-
-    ((number_of_lines_1_has_more_than_2 = $(wc -l <${filenames[0]}) - $(wc -l <${filenames[1]})))
-
-    echo $number_of_lines_1_has_more_than_2
-
-    local file_that_misses_lines_index=1
-
-    if [ $number_of_lines_1_has_more_than_2 -lt 0 ]; then
-        ((number_of_lines_1_has_more_than_2 = -$number_of_lines_1_has_more_than_2))
-        file_that_misses_lines_index=0
-    fi
-
-    local file_that_misses_lines=${filenames[${file_that_misses_lines_index}]}
-
-    # TODO write to file use function
-    echo "File $file_that_misses_lines missing $number_of_lines_1_has_more_than_2 entries." > compare.txt
-
-    random_line=$(head -n 1 $file_that_misses_lines)
-
-    until [ $number_of_lines_1_has_more_than_2 -eq 0 ]; do
-        echo $random_line >>$file_that_misses_lines
-        let number_of_lines_1_has_more_than_2-=1
+        is_sum_odd_or_even $f $odd_even_filename
     done
 
     return 0
