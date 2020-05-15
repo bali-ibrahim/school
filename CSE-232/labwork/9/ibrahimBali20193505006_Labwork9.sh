35a0a962acb0bca94622582f9fa1e790dac6297d
İbrahim Bali <balii@pm.me>
2020-05-15 18:33:40 +0300

    add lab 9

---
+++ CSE-232/labwork/9/ibrahimBali20193505006_Labwork9.sh
@@ -0,0 +1,12 @@
+# ibrahimBali20193505006_Labwork9
+
+input_filename=input_file.txt
+sep="\t"
+
+echo -e "Mark${sep}20${sep}Dirty" >${input_filename}
+echo -e "John${sep}55${sep}Dirty" >>$input_filename
+echo -e "George${sep}40${sep}Clean" >>$input_filename
+echo -e "Michel${sep}38${sep}Clean" >>$input_filename
+echo -e "Tina${sep}55${sep}Clean" >>$input_filename
+echo -e "Pearl${sep}45${sep}Dirty" >>$input_filename
+echo -e "Joseph${sep}66${sep}Dirty" >>$input_filename
